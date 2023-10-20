d7e598b6eb3bcfbe659d67e13590d672d22be160
Kartik Tiwari <mekartik27@gmail.com>
2023-10-21 01:03:13 +0530

    JWT auth

--- server/app.js
+++ server/app.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 const app = express();
 
-// Connect Database
-connectDB();
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
+  // DB Config
+  const db = require("./config/keys").mongoURI;
+  // Connect to MongoDB
+  mongoose
+    .connect(
+      db,
+      { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 8082;
 
